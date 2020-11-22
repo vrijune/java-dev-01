47a000502ac6f1b8534fa0f10e5743d858b66948
Wen YU <wyu861@aucklanduni.ac.nz>
2020-11-22 14:31:40 +0800

    guessinggame

--- src/ictgradschool/industry/controlflow/guessing/GuessingGame.java
+++ src/ictgradschool/industry/controlflow/guessing/GuessingGame.java
@@ -1,12 +1,37 @@
 package ictgradschool.industry.controlflow.guessing;
 
+import ictgradschool.Keyboard;
+
 /**
  * A guessing game!
  */
 public class GuessingGame {
 
     public void start() {
+        int goal = (int) (Math.random()*100+1);
 
+        int guess = 0;
+        while (guess != goal) {
+
+
+            System.out.println("Enter your guess:" + guess);
+            guess = Integer.parseInt(Keyboard.readInput());
+
+            if (guess > goal) ;
+            {
+                System.out.println("Too high, try again");
+            }
+
+          else if (guess < goal) ;
+            {
+                System.out.println("Too low, try again");
+            }
+
+          else{
+                System.out.println("Perfect!!");
+            }
+            System.out.println("Goodbye");
+        }
         // TODO Write your code here.
     }
 
